4304fe333ad05054bfaffd6792b2bd45321db5fc
Cooper <55swimmer55@gmail.com>
2023-10-10 14:26:10 -0400

    completed mock code challenge via walkthrough

--- lib/classes/Coffee.py
+++ lib/classes/Coffee.py
@@ -1,15 +1,30 @@
 class Coffee:
     def __init__(self, name):
         self.name = name
+
+        self._orders = []
+        self._customers = []
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if type(name) == str and len(name) > 2 and not hasattr(self, "name"):
+            self._name = name
+        else:
+            raise Exception("Name must be a string greater than 2 characters!")
+            
         
     def orders(self):
-        pass
+        return self._orders
     
     def customers(self):
-        pass
+        return list(set(self._customers))
     
     def num_orders(self):
-        pass
+        return len(self._orders)
     
     def average_price(self):
-        pass
+        return sum([order.price for order in self._orders]) / len(self._orders)
--- lib/classes/Customer.py
+++ lib/classes/Customer.py
@@ -1,12 +1,50 @@
 class Customer:
+
+    all = []
+
     def __init__(self, name):
         self.name = name
+
+        self._orders = []
+        self._coffees = []
+
+        Customer.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+        
+    @name.setter
+    def name(self, name):
+        if type(name) == str and 1 <= len(name) <= 15:
+            self._name = name
+        else:
+            raise Exception("Name must be a string between 1 and 15 characters!")
         
     def orders(self):
-        pass
+        return self._orders
     
     def coffees(self):
-        pass
+        return list(set(self._coffees))
     
     def create_order(self, coffee, price):
-        pass
+        from classes.Order import Order
+
+        return Order(self, coffee, price)
+    
+    @classmethod
+    def most_aficionado(cls, coffee):
+        customer_amount_spent = {}
+
+        for customer in cls.all:
+            for order in customer._orders:
+                if order.coffee == coffee:
+                    if customer in customer_amount_spent:
+                        customer_amount_spent[customer] += order.price
+                    else:
+                        customer_amount_spent[customer] = order.price
+
+        if len(customer_amount_spent) == 0:
+            return None
+        else:
+            return max(customer_amount_spent, key = customer_amount_spent.get)
